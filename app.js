7a7ca8a9d33f7fa6b43908280d209f91a343d30b
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-02-26 07:57:55 +0000

    basic card functionality established

---
+++ app.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    console.log("JS Connected!");
+
+    // Arrays for the cards
+    const symbolArr = ["♦", "♥", "♠", "♣"];
+    const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+    // Random number generation for arrays 
+    let symbolIndex = Math.floor(Math.random() * symbolArr.length);
+    let numberIndex = Math.floor(Math.random() * numberArr.length);
+
+    // Access to the divs
+    let topSymbolElement = document.getElementById("top-symbol");
+    let bottomSymbolElement = document.getElementById("bottom-symbol");
+    let numberElement = document.getElementById("card-number");
+
+
+    // Inner html for substitution
+
+    topSymbolElement.innerHTML = symbolArr[symbolIndex];
+    bottomSymbolElement.innerHTML = symbolArr[symbolIndex];
+    numberElement.innerHTML = numberArr[numberIndex]; 
+
+    // Conditional rendering for smybol colors
+    if (symbolArr[symbolIndex] === "♥" || symbolArr[symbolIndex] === "♦") {
+        topSymbolElement.style.color = "red";
+        bottomSymbolElement.style.color = "red";
+    };
+
+};
+
+
